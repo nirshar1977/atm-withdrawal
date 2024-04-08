42eb8a337591d7e2abd82416f818b4cb4f26e20b
Nir Sharabi <nirby7@gmail.com>
2024-04-08 17:29:08 +0300

    Service Impl UTs

---
+++ src/test/java/com/bankhapoalim/atmwithdrawal/service/WithdrawalServiceImplTest.java
@@ -0,0 +1,71 @@
+package com.bankhapoalim.atmwithdrawal.service;
+
+import com.bankhapoalim.atmwithdrawal.dto.WithdrawalRequestDTO;
+import com.bankhapoalim.atmwithdrawal.entity.BankAccount;
+import com.bankhapoalim.atmwithdrawal.repository.BankAccountRepository;
+import com.bankhapoalim.atmwithdrawal.repository.WithdrawalRequestRepository;
+import com.bankhapoalim.atmwithdrawal.service.BankAccountServiceImpl;
+import com.bankhapoalim.atmwithdrawal.util.ValidationUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class WithdrawalServiceImplTest {
+
+    @Mock
+    private ValidationUtils validationUtils;
+
+    @Mock
+    private WithdrawalRequestRepository withdrawalRequestRepository;
+
+    @Mock
+    private BankAccountRepository accountRepository;
+
+    @Mock
+    private BankAccountServiceImpl bankAccountService;
+
+    @InjectMocks
+    private WithdrawalServiceImpl withdrawalService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testProcessWithdrawalRequest_Success() {
+        // Mocking the validation check to return true
+        when(validationUtils.isValidWithdrawalRequest()).thenReturn(withdrawalRequestDTO -> true);
+
+        WithdrawalRequestDTO withdrawalRequestDTO = new WithdrawalRequestDTO();
+        withdrawalRequestDTO.setCardNumber("1234567890123456");
+        withdrawalRequestDTO.setSecretCode("1234");
+        withdrawalRequestDTO.setAmount(BigDecimal.valueOf(100.0));
+
+        BankAccount mockBankAccount = new BankAccount();
+        mockBankAccount.setAccountId(1L);
+        mockBankAccount.setBalance(BigDecimal.valueOf(1000.0));
+
+        // Mock the bankAccountService to return the mockBankAccount
+        when(bankAccountService.getAccountFromCache(any())).thenReturn(mockBankAccount);
+
+        // Perform the method call
+        boolean result = withdrawalService.processWithdrawalRequest(withdrawalRequestDTO);
+
+        // Verify that the save method was called with the correct parameters
+        verify(withdrawalRequestRepository, times(1)).save(any());
+        verify(accountRepository, times(1)).save(any());
+
+        // Assert the result
+        assert(result);
+    }
+}
