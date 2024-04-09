3084a0840ae69852947b4283bf33d43b93b0238d
Nir Sharabi <nirby7@gmail.com>
2024-04-09 14:09:54 +0300

    Fix Controller tests

--- src/test/java/com/bankhapoalim/atmwithdrawal/controller/WithdrawalControllerTest.java
+++ src/test/java/com/bankhapoalim/atmwithdrawal/controller/WithdrawalControllerTest.java
@@ -1,20 +1,26 @@
 package com.bankhapoalim.atmwithdrawal.controller;
 
+import com.bankhapoalim.atmwithdrawal.AtmWithdrawalApplication;
 import com.bankhapoalim.atmwithdrawal.dto.WithdrawalRequestDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.math.BigDecimal;
 
-@SpringBootTest
+@SpringBootTest(classes = AtmWithdrawalApplication.class)
 @AutoConfigureMockMvc
 class WithdrawalControllerTest {
 
